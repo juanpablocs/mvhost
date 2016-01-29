7b835f68f47957e7c77f12c903c627ab00dc5122
Andy Ecca <andy.ecca@gmail.com>
2016-01-29 15:03:22 -0500

    refactor(core): Refactor structure and models for virtual hosts

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-#!/usr/bin/env node
-var program     = require('commander');
-var createVhost = require('./bin/create-vhost');
-var removeVhost = require('./bin/remove-vhost');
+var apacheVhost = require('./apache');
 
-program
-  .version('0.0.1')
-  .option('-r, --remove', 'remove virtualhost, this not remove proyect (danger: this action is not reverse)')
-  .option('-c, --create', 'Add domain in virtualhost')
-  .parse(process.argv);
+var userRoot = process.getuid && process.getuid() === 0;
 
-if(program.create){
-  new createVhost();
-}else if(program.remove){
-  new removeVhost(function(err){
-    if(err){
-      console.log('error:');
-      console.log(' ', err);
-    }else{
-      console.log('task is finish!!!');
-    }
-  });
-}else{
-  new createVhost();
+
+if( !userRoot ) {
+  console.log("You have no permission to run as non-root user. Use sudo");
+  return;
 }
+
+
+var st = new apacheVhost().create('gatos1.com', '~/Projects/aecca');
+
+//
+console.log(st);
