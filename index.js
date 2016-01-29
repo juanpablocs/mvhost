1c16c04a81d6fde2e3bf58ee1117fd9f2a76fe6d
Juan Pablo <juanpablocs21@gmail.com>
2016-01-29 17:48:35 -0500

    refactor(core): se agrego cli commander y se hicieron modificaciones

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+#!/usr/bin/env node
 var apacheVhost = require('./apache');
+var path        = require('path');
+var program     = require('commander');
+var inquirer    = require('inquirer');
 
 var userRoot = process.getuid && process.getuid() === 0;
 
@@ -8,8 +12,28 @@ if( !userRoot ) {
   return;
 }
 
+program
+  .version('0.0.1')
 
-var st = new apacheVhost().create('gatos1.com', '~/Projects/aecca');
+  .command('create <domain>')
+  .description('create domain.com for create virtualhost')
+  .action(function(domain){
+    new apacheVhost().create(domain, program.path);
+  });
 
-//
-console.log(st);
+program
+  .option('-r, --remove', 'remove virtualhost, this not remove proyect (danger: this action is not reverse)')
+  .option('-c, --create', 'Add domain in virtualhost')
+  .option('-p, --path', 'path folder')
+
+  .parse(process.argv);
+
+// if(program.create){
+//   new createVhost(myCallback);
+// }else if(program.remove){
+//   new removeVhost(myCallback);
+// }else{
+//   new createVhost(myCallback);
+// }
+
+
